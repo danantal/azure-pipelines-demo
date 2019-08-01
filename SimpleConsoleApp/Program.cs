510f34f292c3fd76194dae8d0b7cf7922f3c1035
Dan Antal <dan@templafy.com>
2019-08-01 10:44:15 +0300

    Added simple console app with test project

---
+++ SimpleConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
